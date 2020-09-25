0c6f56018fbe13fe9b1925ba07c76cb2a647ebe1
Anita <anita.lim@ontariotechu.net>
2020-09-25 17:27:55 -0400

    Week 3 Done

--- samples/INFR-1350U/Week3-Starter/src/main.cpp
+++ samples/INFR-1350U/Week3-Starter/src/main.cpp
@@ -145,6 +145,33 @@ int main() {
 	 BufferAttribute{ 1, 3, GL_FLOAT, false, 0, NULL }
 		});
 
+	static const float interleaved[] = {
+		// X      Y     Z     R     G     B
+		  0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f,
+		  0.5f, 0.5f, 0.5f, 0.3f, 0.2f, 0.5f,
+		 -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+		  0.5f, 1.0f, 0.5f, 1.0f, 1.0f, 1.0f
+	};
+
+	VertexBuffer* interleaved_vbo = new VertexBuffer();
+	interleaved_vbo->LoadData(interleaved, 6 * 4);
+
+	static const uint16_t indices[] = {
+		0, 1, 2,
+		1, 3, 2
+	};
+
+	IndexBuffer* interleaved_ibo = new IndexBuffer();
+	interleaved_ibo->LoadData(indices, 3 * 2);
+
+	size_t stride = sizeof(float) * 6;
+	VertexArrayObject* vao2 = new VertexArrayObject();
+	vao2->AddVertexBuffer(interleaved_vbo, {
+		BufferAttribute(0, 3, GL_FLOAT, false, stride, 0),
+		BufferAttribute(1, 3, GL_FLOAT, false, stride, sizeof(float) * 3),
+	});
+	vao2->SetIndexBuffer(interleaved_ibo);
+
 	// Load our shaders
 
 	/*if (!loadShaders())
@@ -182,10 +209,17 @@ int main() {
 		vao->Bind();
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+		vao2->Bind();
+		glDrawElements(GL_TRIANGLES, interleaved_ibo->GetElementCount(), interleaved_ibo->GetElementType(), nullptr);
+		vao->UnBind();
+
 		glfwSwapBuffers(window);
 	}
 
 	delete shader;
+	delete interleaved_ibo;
+	delete vao2;
+	delete interleaved_vbo;
 	delete vao;
 	delete posVbo;
 	delete color_vbo;
